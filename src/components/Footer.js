8cb6ce4d1714363334b45742eab4f1a27239f306
andyjalex <36079847+andyjalex@users.noreply.github.com>
2024-11-25 15:18:37 +0000

    navbar and footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,89 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../assets/logo.svg'
+
 const Footer = () => {
-    return <div>Footer</div>
+    return (
+		<Wrapper>
+			<div className="mainSection">
+				<div className="leftSide">
+					<div className="main-header__brand" to='/'>
+							<img src={logo} alt='COBRAS' />
+
+						</div>
+				</div>
+				<div className="rightSide">
+				<ul className="nav-links">
+          <li><a href="/" data-text="about">About</a></li>
+          <li><a href="/tournaments" data-text="tournaments">Tournaments</a></li>
+          <li><a href="/social" data-text="social">Social</a></li>
+          <li><a href="/gallery" data-text="gallery">Gallery</a></li>
+          <li><a href="/contact" data-text="contact">Contact</a></li>
+        </ul>
+				</div>
+			</div>
+			<div className="bottomSection">
+			<h3>&copy; {new Date().getFullYear()}<span> Cobras BVBC</span></h3>
+			</div>
+			
+
+			
+			
+		</Wrapper>
+		)
 }
 
+const Wrapper = styled.section`
+
+	display: flex;
+	flex-direction: column;
+	padding:2rem;
+	text-align: center;
+	background-color: black;
+
+	.mainSection {
+		display: flex;
+		justify-content: space-between;
+		margin: 0 4rem;
+		width: 100%;
+	}
+
+	.leftSide {
+		display: flex;
+
+	}
+	.rightSide {
+		display:flex;
+	}
+
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		list-style-type: none;
+		padding: 0;
+
+
+	}
+
+	ul li {
+		width: 50%; /* Each item takes up half the container width */
+  	box-sizing: border-box; /* Ensures padding is included in width */
+	}
+
+	a {
+		color: white;
+		text-decoration: none;
+		font-size: 1.2rem;	
+	}
+
+	h3 {
+		color: white;
+	}
+
+	@media (min-width: 992px) {
+	.mainSection {
+		width: 90%;
+	}
+	}
+  `
 export default Footer;
